ce4c4e7d46eaa81c8b38e5e1e2dfd5b2e9906b28
roeibadur <51096879+roeibadur@users.noreply.github.com>
2021-04-21 15:53:26 +0300

    first commit

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# CourseProject
+# RecipeBook
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 9.0.7.
 
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,6 +20,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('course-project app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('Recipe-Book app is running!');
   });
 });
